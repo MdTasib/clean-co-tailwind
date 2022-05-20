97168391c9017c9ede485158cd9eefd7b1c4d963
MdTasib <ohidulalam442@gmail.com>
2022-05-20 12:32:21 +0600

    services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import fetecher from "../api";
 
 const Services = () => {
+	const [services, setServices] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const res = await fetecher.get("/service");
+			setServices(res.data);
+		})();
+	}, []);
+
+	console.log(services);
 	return <div>this is services page</div>;
 };
 
